69763a470f6cb65c1c26ad36d6a5020593a602dc
nstranges <nicholastranges4@hotmail.com>
2025-03-09 17:38:22 -0400

    Got the test controller to compile.

--- Controls/Controller.ino/Controller.ino.ino
+++ Controls/Controller.ino/Controller.ino.ino
@@ -2,18 +2,15 @@
 
 // Flex pin attachments. CHANGE FOR ESP
 const int FLEX_PIN1 = A0;
-const int FLEX_PIN1 = A1;
-const int FLEX_PIN1 = A2;
+const int FLEX_PIN2 = A1;
+const int FLEX_PIN3 = A2;
 
 // Holds tuning values
 struct params {
-  kp;
-  ki;
-  kd;
-}
-params pidParams1;
-params pidParams2;
-params pidParams3;
+  double Kp;
+  double Ki;
+  double Kd;
+};
 
 // ADJUST USING THE MAP
 double setpoint1;
@@ -29,22 +26,14 @@ double flexADC3;
 double motorPos3;
 
 // PID tuning values
-pidParams1.Kp = 5.884;
-pidParams1.Ki = 27.125; 
-pidParams1.Kd = 0;
-
-pidParams2.Kp = 5.884;
-pidParams2.Ki = 27.125; 
-pidParams2.Kd = 0;
-
-pidParams3.Kp = 5.884;
-pidParams3.Ki = 27.125; 
-pidParams3.Kd = 0;
+params pidParams1 = {5.884, 27.125, 0.0};
+params pidParams2 = {5.884, 27.125, 0.0};
+params pidParams3 = {5.884, 27.125, 0.0};
 
 // Create PID objects
-PID loop1(&flexADC1, &motorPos1, &setpoint1, Kp, Ki, Kd, DIRECT);
-PID loop2(&flexADC2, &motorPos2, &setpoint2, Kp, Ki, Kd, DIRECT);
-PID loop3(&flexADC3, &motorPos3, &setpoint3, Kp, Ki, Kd, DIRECT);
+PID loop1(&flexADC1, &motorPos1, &setpoint1, pidParams1.Kp, pidParams1.Ki, pidParams1.Kd, DIRECT);
+PID loop2(&flexADC2, &motorPos2, &setpoint2, pidParams2.Kp, pidParams2.Ki, pidParams2.Kd, DIRECT);
+PID loop3(&flexADC3, &motorPos3, &setpoint3, pidParams3.Kp, pidParams3.Ki, pidParams3.Kd, DIRECT);
 
 void setup() {
     Serial.begin(9600);
@@ -62,9 +51,8 @@ void setup() {
 
 void loop() {
     flexADC1 = analogRead(FLEX_PIN1);
-    Serial.println("Input: " + String(flexADC));
 
-    loop1.compute();
+    loop1.Compute();
     
     delay(250);
 }
